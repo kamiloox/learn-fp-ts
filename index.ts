638ff431fd36f8d14833b9830a5f0f21ab648a00
kamiloox <kamiltroczewski@gmail.com>
2022-08-27 19:16:54 +0200

    feat: explore higher kinded types

--- index.ts
+++ index.ts
@@ -13,3 +13,19 @@ const calcSomething = flow(inc, multiplyBy2);
 const result = calcSomething(4);
 
 console.log(result);
+
+interface URIToKind<A> {
+  Array: ReadonlyArray<A>;
+}
+
+type URIS = keyof URIToKind<unknown>;
+
+type Kind<F extends URIS, A> = URIToKind<A>[F];
+
+interface Mappable<F extends URIS> {
+  readonly map: <A, B>(f: (a: A) => B) => (as: Kind<F, A>) => Kind<F, B>;
+}
+
+const mappableArray: Mappable<'Array'> = {
+  map: (f) => (as) => as.map(f),
+};
